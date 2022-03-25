cb4ae50fddb1de84beb54bd60653b42821c2e459
Eryk Juszczak <juszczakeryk8@gmail.com>
2022-03-25 18:57:18 +0100

    Few Litte fixes to nav component

--- src/pages/destination/Destination.jsx
+++ src/pages/destination/Destination.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../../components/nav/Navbar'
 const Destination = ({ Data }) => {
+	console.log(Data)
 	return (
 		<div className='destination'>
 			<Navbar page='destination' />
+			
 		</div>
 	)
 }
--- src/pages/technology/Technology.jsx
+++ src/pages/technology/Technology.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/nav/Navbar'
 const Technology = ({ Data }) => {
+	console.log(Data)
 	return (
 		<div className='technology'>
 			<Navbar page='technology' />
