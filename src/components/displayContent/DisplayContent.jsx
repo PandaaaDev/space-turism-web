12bd2254850de01d7703b0c9d28cfd8e45ea3696
Eryk Juszczak <juszczakeryk8@gmail.com>
2022-03-25 19:54:37 +0100

    First Concept of displaying Data for pages

---
+++ src/components/displayContent/DisplayContent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DisplayContent = ({ data }) => {
+	return <div>{data.name}</div>
+}
+
+export default DisplayContent
--- src/pages/destination/Destination.jsx
+++ src/pages/destination/Destination.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/nav/Navbar'
+import DisplayContent from '../../components/displayContent/DisplayContent'
+import './destination.scss'
 const Destination = ({ Data }) => {
-	console.log(Data)
+	const [display, setDisplay] = useState(0)
+	const displayChangeHanlder = () => {
+		setDisplay(1)
+		console.log(display)
+	}
+	const displayChangeHanlder2 = () => {
+		setDisplay(0)
+		console.log(display)
+	}
 	return (
 		<div className='destination'>
 			<Navbar page='destination' />
-			
+			<div className='SliderBtn' onClick={displayChangeHanlder}>
+				klikacz
+			</div>
+			<div className='SliderBtn' onClick={displayChangeHanlder2}>
+				klikacz 2
+			</div>
+			<DisplayContent data={Data[display]} display={display} />
 		</div>
 	)
 }
