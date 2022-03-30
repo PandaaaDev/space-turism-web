dbcbaa10834c3c6b1086099c3814ada42164711a
Eryk Juszczak <juszczakeryk8@gmail.com>
2022-03-30 12:06:33 +0200

    Technology display setup.

--- src/pages/technology/Technology.jsx
+++ src/pages/technology/Technology.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DisplayImg from '../../components/displayImg/DisplayImg'
 import Navbar from '../../components/nav/Navbar'
+import Slide from '../../components/slide/Slide'
 const Technology = ({ Data }) => {
-	console.log(Data)
+	const [display, setDisplay] = useState(0)
+	const displayHandler = id => {
+		setDisplay(id)
+	}
 	return (
 		<div className='technology'>
 			<Navbar page='technology' />
+			<div className='technologyTitle'></div>
+			<div className='technologyContainer'>
+				<div className='left'>
+					<Slide data={Data} displayState={display} changeSlide={displayHandler} />
+					<DisplayTechnologyContent data={Data[display]} />
+				</div>
+				<div className='right'>
+					<DisplayImg className={technologyImg} data={Data[display]} />
+				</div>
+			</div>
 		</div>
 	)
 }
